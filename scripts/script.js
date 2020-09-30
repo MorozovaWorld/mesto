24d3294c2d4de8bb7973c815ca210751d164d327
Ekaterina Morozova <emorozova1@yandex.ru>
2020-09-30 22:00:15 +0300

    feat: написать функция открытия и закрытия попапа

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector('.popup');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
