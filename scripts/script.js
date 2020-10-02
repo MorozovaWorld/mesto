0745f569218cd8733cfb848edb79dcfa307c91f0
Ekaterina Morozova <emorozova1@yandex.ru>
2020-10-02 22:00:40 +0300

    feat: сделать проект Место

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,36 @@
+console.log('Hello world')
+
 let popup = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.profile__popup-button-open');
-let popupCloseBtn = document.querySelector('.popup__close');
+let popupCloseBtn = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.form');
+let nameInput = formElement.querySelector('#name');
+let jobInput = formElement.querySelector('#job');
 
 function popupOpen() {
   popup.classList.add('popup_opened');
 }
 
 function popupClose() {
+  nameInput.value="";
+  jobInput.value="";
   popup.classList.remove('popup_opened');
+
 }
 
 popupOpenBtn.addEventListener('click', popupOpen);
 popupCloseBtn.addEventListener('click', popupClose);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let nameInputNew = document.querySelector('.profile__info-title');
+    let jobInputNew = document.querySelector('.profile__info-subtitle');
+
+    nameInputNew.textContent = nameInput.value;
+    jobInputNew.textContent = jobInput.value;
+
+    popupClose();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
