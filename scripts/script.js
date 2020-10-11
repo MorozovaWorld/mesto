71d2b64161655d9aa7b06375c3b7d33aa154f915
Ekaterina Morozova <emorozova1@yandex.ru>
2020-10-11 16:54:27 +0300

    feat: initialCards from template

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,52 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cards = document.querySelector('.cards');
+const template = document.querySelector('.template');
+
+const renderCards = () => {
+    const items = initialCards.map(element => getItems(element));
+    cards.append(...items)
+};
+
+const getItems = (element) => {
+    const card = template.content.cloneNode(true);
+    cardText = card.querySelector('.card__caption-text');
+    cardImg = card.querySelector('.card__img');
+
+    cardText.textContent = element['name'];
+    cardImg.setAttribute('src', element['link']);
+
+    return card;
+};
+
+renderCards();
+
